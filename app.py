dc147113ac7d65e9dd3cce3143cc7caaea51205f
Vijayapandian <35286599+Vijayapandianr@users.noreply.github.com>
2021-06-02 16:51:43 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask           
+app = Flask(__name__)             
+
+@app.route("/")                   
+def hello(): 
+    return """
+          <h1>Python Flask in Docker!</h1>
+          <p>A sample web-app for running Flask inside Docker.</p>
+          """
+if __name__ == "__main__":       
+    app.run()     
